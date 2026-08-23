9dcbd10bb7f9e64da41f3618dcf950ae250a57f4
🌠 韓濬氾 <148828396+fullgukbap@users.noreply.github.com>
2026-08-23 02:56:13 +0000

    feat: create config.go
    환경변수 설정 파일을 위해 작성함

---
+++ pkg/config/config.go
@@ -0,0 +1,100 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct {
+	Env    string    `yaml:"env"`
+	Name   string    `yaml:"name"`
+	Listen string    `yaml:"listen"`
+	sqlite RDBConfig `yaml:"sqlite"`
+}
+
+func New(path string) (*Config, error) {
+	config := &Config{}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open: %v", err)
+	}
+	defer file.Close()
+
+	err = yaml.NewDecoder(file).Decode(config)
+	if err != nil {
+		return nil, fmt.Errorf("failed new decode: %v", err)
+	}
+
+	return config, nil
+}
+
+type Env uint8
+
+const (
+	EnvDev Env = iota
+	EnvQa
+	EnvProd
+)
+
+// UnmarshalYAML 인터페이스를 구현하여 문자열을 읽고 내부적으로 정수형(int) enum으로 변환합니다.
+func (e *Env) UnmarshalYAML(value *yaml.Node) error {
+	var decodedValue string
+	if err := value.Decode(&decodedValue); err != nil {
+		return err
+	}
+
+	switch decodedValue {
+	case "dev", "development":
+		*e = EnvDev
+	case "qa":
+		*e = EnvQa
+	case "prod", "production":
+		*e = EnvProd
+	default:
+		return fmt.Errorf("unknown env value: %s", decodedValue)
+	}
+
+	return nil
+}
+
+type RDBConfig struct {
+	Host     string     `yaml:"host"`
+	User     string     `yaml:"user"`
+	Port     int        `yaml:"port"`
+	Password string     `yaml:"password"`
+	DBname   string     `yaml:"dbName"`
+	Options  RDBOptions `yaml:"options"`
+}
+
+type RDBOptions struct {
+	MaxIdleConns    int           `yaml:"maxIdleConns"`
+	MaxOpenConns    int           `yaml:"maxOpenConns"`
+	ConnMaxLifetime time.Duration `yaml:"connMaxLifetime"`
+}
+
+func (c *RDBOptions) UnmarshalYAML(value *yaml.Node) error {
+	var raw struct {
+		MaxIdleConns    int    `yaml:"maxIdleConns"`
+		MaxOpenConns    int    `yaml:"maxOpenConns"`
+		ConnMaxLifetime string `yaml:"connMaxLifetime"`
+	}
+
+	if err := value.Decode(&raw); err != nil {
+		return err
+	}
+
+	c.MaxIdleConns = raw.MaxIdleConns
+	c.MaxOpenConns = raw.MaxOpenConns
+
+	duration, err := time.ParseDuration(raw.ConnMaxLifetime)
+	if err != nil {
+		return err
+	}
+	c.ConnMaxLifetime = duration
+
+	return nil
+}
