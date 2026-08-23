90fd9472c6d2410fb5391fcc7f3e740d361e5b8b
🌠 韓濬氾 <148828396+fullgukbap@users.noreply.github.com>
2026-08-23 03:48:00 +0000

    feat: create user domain

---
+++ internal/domain/user.go
@@ -0,0 +1,11 @@
+package domain
+
+import (
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Nickname  string  `gorm:"column:nickname;type:varchar(255);unique"` // min(2), max(12)
+	Biography *string `gorm:"column:biography;type:varchar(255);"`      // min(0), max(14)
+}
