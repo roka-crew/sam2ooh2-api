d94cb8bb61e293f79889d4df7522e5bfe7f3006d
🌠 韓濬氾 <148828396+fullgukbap@users.noreply.github.com>
2026-08-24 10:21:18 +0000

    feat: define group store interface

--- internal/domain/group_domain.go
+++ internal/domain/group_domain.go
@@ -1,10 +1,13 @@
 package domain
 
 import (
+	"context"
+
+	"github.com/roka-crew/sam2ooh2-api/internal/payload"
 	"gorm.io/gorm"
 )
 
-type Gruops []Group
+type Groups []Group
 
 type Group struct {
 	gorm.Model
@@ -19,4 +22,17 @@ type Group struct {
 
 	// Group < 1:N > Chapter : 그룹 내에 생성된 챕터들
 	Chapters []Chapter `gorm:"foreignKey:GroupID;constraint:OnDelete:CASCADE;"`
+}
+
+type GroupStore interface {
+	CreateGroup(ctx context.Context, param payload.CreateGroupParam) (Group, error)
+	ListGroups(ctx context.Context, param payload.ListGroupsParam) (Groups, error)
+	PatchGroup(ctx context.Context, param payload.PatchGroupParam) error
+	DeleteGroup(ctx context.Context, param payload.DeleteGroupParam) error
+
+	// N:M 멤버십 조작 API
+	AddUserToGroup(ctx context.Context, param payload.AddUserToGroupParam) error
+	RemoveUserFromGroup(ctx context.Context, param payload.RemoveUserFromGroup) error
+	CountGroupMembers(ctx context.Context, param payload.CountGroupUsers) (int, error)
+	IsUserInGroup(ctx context.Context, param payload.IsUserInGroup) (bool, error)
 }
---
+++ internal/payload/group_payload.go
@@ -0,0 +1,68 @@
+package payload
+
+import "github.com/roka-crew/sam2ooh2-api/pkg/query"
+
+// store layer
+type CreateGroupParam struct {
+	Title       string
+	Author      string
+	PageCount   int
+	Publisher   string
+	Description string
+}
+
+type ListGroupsParam struct {
+	// conditions
+	IDs          []uint
+	Titles       []string
+	Authors      []string
+	PageCounts   []int
+	Publishers   []string
+	Descriptions []string
+
+	// sort
+	Sorts query.Sort
+
+	// options
+	Limit  int
+	Offset int
+}
+
+type PatchGroupParam struct {
+	// conditions
+	ID uint
+
+	// fields
+	Title       string
+	Author      string
+	PageCount   int
+	Publisher   string
+	Description string
+}
+
+type DeleteGroupParam struct {
+	// conditions
+	ID uint
+
+	// options
+	IsHardDelete bool
+}
+
+type AddUserToGroupParam struct {
+	UserID  uint
+	GroupID uint
+}
+
+type RemoveUserFromGroup struct {
+	UserID  uint
+	GroupID uint
+}
+
+type CountGroupUsers struct {
+	GroupID uint
+}
+
+type IsUserInGroup struct {
+	UserID  uint
+	GroupID uint
+}
